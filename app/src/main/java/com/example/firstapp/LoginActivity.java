3fe109f88c10732994fa6860d73abe8a0e463d0f
meshnesh <tonnie.nesh@gmail.com>
2019-01-28 19:39:39 +0300

    Feature(Implement login): -Implement and user authentication using firebase. (Finish user login)

--- app/src/main/java/com/example/firstapp/LoginActivity.java
+++ app/src/main/java/com/example/firstapp/LoginActivity.java
@@ -1,6 +1,7 @@
 package com.example.firstapp;
 
 import android.content.Intent;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
@@ -12,12 +13,19 @@ import android.widget.ProgressBar;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
 public class LoginActivity extends AppCompatActivity {
 
     private static final String TAG = "LoginActivity";
 
     //Firebase
-
+    private FirebaseAuth.AuthStateListener mAuthListener;
 
     // widgets
     private EditText mEmail, mPassword;
@@ -31,7 +39,7 @@ public class LoginActivity extends AppCompatActivity {
         mPassword = (EditText) findViewById(R.id.password);
         mProgressBar = (ProgressBar) findViewById(R.id.progressBar);
 
-
+        setupFirebaseAuth();
 
         Button signIn = (Button) findViewById(R.id.email_sign_in_button);
         signIn.setOnClickListener(new View.OnClickListener() {
@@ -43,10 +51,26 @@ public class LoginActivity extends AppCompatActivity {
                         && !isEmpty(mPassword.getText().toString())){
                     Log.d(TAG, "onClick: attempting to authenticate.");
 
+                    showDialog();
 
-                }else{
+                    FirebaseAuth.getInstance().signInWithEmailAndPassword(mEmail.getText().toString(),
+                            mPassword.getText().toString())
+                            .addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                                @Override
+
+                                public void onComplete(@NonNull Task<AuthResult> task) {
+                                    hideDialog();
+                                }
+                            }).addOnFailureListener(new OnFailureListener() {
+                        @Override
+                        public void onFailure(@NonNull Exception e) {
+                            Toast.makeText(LoginActivity.this, "Authentication Failed", Toast.LENGTH_SHORT).show();
+                        }
+                    });
+                } else {
                     Toast.makeText(LoginActivity.this, "You didn't fill in all the fields.", Toast.LENGTH_SHORT).show();
                 }
+
             }
         });
 
@@ -108,4 +132,31 @@ public class LoginActivity extends AppCompatActivity {
         ----------------------------- Firebase setup ---------------------------------
      */
 
+    private void setupFirebaseAuth() {
+        mAuthListener = new FirebaseAuth.AuthStateListener() {
+            @Override
+            public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {
+                FirebaseUser user = firebaseAuth.getCurrentUser();
+
+                if (user != null){
+                    Log.d(TAG, "onAuthStateChanged: signed_in " + user.getUid());
+                    Toast.makeText(LoginActivity.this, "signed_in successfully ", Toast.LENGTH_SHORT).show();
+                }Log.d(TAG, "onAuthStateChanged: signed_out ");
+            }
+        };
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        FirebaseAuth.getInstance().addAuthStateListener(mAuthListener);
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        if (mAuthListener!=null){
+            FirebaseAuth.getInstance().removeAuthStateListener(mAuthListener);
+        }
+    }
 }
