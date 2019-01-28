ac01d298cca1fae7ec9b858f0d89f20300d8eab0
Antony <tonnie.nesh@gmail.com>
2019-01-28 23:02:50 +0300

    Feature implement user registration && Login using Firebase Auth (#1)
    * Feature(Register new user email):
    - It registers a new user email and save it to the firebase app account.
    (finishes the application)
    
    * Feature(Implement login):
    -Implement and user authentication using firebase.
    (Finish user login)

---
+++ app/src/main/java/com/example/firstapp/RegisterActivity.java
@@ -0,0 +1,159 @@
+package com.example.firstapp;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.View;
+import android.view.WindowManager;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ProgressBar;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+
+public class RegisterActivity extends AppCompatActivity {
+
+    private static final String TAG = "RegisterActivity";
+
+    private static final String DOMAIN_NAME = "andela.com";
+
+    //widgets
+    private EditText mEmail, mPassword, mConfirmPassword;
+    private Button mRegister;
+    private ProgressBar mProgressBar;
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.register_activity);
+        mEmail = (EditText) findViewById(R.id.input_email);
+        mPassword = (EditText) findViewById(R.id.input_password);
+        mConfirmPassword = (EditText) findViewById(R.id.input_confirm_password);
+        mRegister = (Button) findViewById(R.id.btn_register);
+        mProgressBar = (ProgressBar) findViewById(R.id.progressBar);
+
+        mRegister.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Log.d(TAG, "onClick: attempting to register.");
+
+                //check for null valued EditText fields
+                if(!isEmpty(mEmail.getText().toString())
+                        && !isEmpty(mPassword.getText().toString())
+                        && !isEmpty(mConfirmPassword.getText().toString())){
+
+                    //check if user has a company email address
+                    if(isValidDomain(mEmail.getText().toString())){
+
+                        //check if passwords match
+                        if(doStringsMatch(mPassword.getText().toString(), mConfirmPassword.getText().toString())){
+                            registerNewEmail(mEmail.getText().toString(), mPassword.getText().toString());
+                        }else{
+                            Toast.makeText(RegisterActivity.this, "Passwords do not Match", Toast.LENGTH_SHORT).show();
+                        }
+                    } else{
+                        Toast.makeText(RegisterActivity.this, "Please Register with Company Email", Toast.LENGTH_SHORT).show();
+                    }
+
+                }
+                Toast.makeText(RegisterActivity.this, "You must fill out all the fields", Toast.LENGTH_SHORT).show();
+
+            }
+        });
+
+        hideSoftKeyboard();
+
+    }
+
+    private void registerNewEmail(String email, String password) {
+        showDialog();
+
+        FirebaseAuth.getInstance().createUserWithEmailAndPassword(email, password).addOnCompleteListener(
+                new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        Log.d(TAG, "oncomplete: onComplete" + task.isComplete());
+
+                        if (task.isSuccessful()){
+                            Log.d(TAG, "oncomplete: AuthState" + FirebaseAuth.getInstance().getCurrentUser()
+                                    .getUid());
+
+                            FirebaseAuth.getInstance().signOut();
+                        }
+                        else {
+                            Toast.makeText(RegisterActivity.this, "Unable to Register",
+                                    Toast.LENGTH_SHORT).show();
+                        }
+                        hideDialog();
+                    }
+                }
+        );
+    }
+
+
+    /**
+     * Returns True if the user's email contains '@andela.com'
+     * @param email
+     * @return
+     */
+    private boolean isValidDomain(String email){
+        Log.d(TAG, "isValidDomain: verifying email has correct domain: " + email);
+        String domain = email.substring(email.indexOf("@") + 1).toLowerCase();
+        Log.d(TAG, "isValidDomain: users domain: " + domain);
+        return domain.equals(DOMAIN_NAME);
+    }
+
+    /**
+     * Redirects the user to the login screen
+     */
+    private void redirectLoginScreen(){
+        Log.d(TAG, "redirectLoginScreen: redirecting to login screen.");
+
+        Intent intent = new Intent(RegisterActivity.this, LoginActivity.class);
+        startActivity(intent);
+        finish();
+    }
+    /**
+     * Return true if @param 's1' matches @param 's2'
+     * @param s1
+     * @param s2
+     * @return
+     */
+    private boolean doStringsMatch(String s1, String s2){
+        return s1.equals(s2);
+    }
+
+    /**
+     * Return true if the @param is null
+     * @param string
+     * @return
+     */
+    private boolean isEmpty(String string){
+        return string.equals("");
+    }
+
+
+    private void showDialog(){
+        mProgressBar.setVisibility(View.VISIBLE);
+
+    }
+
+    private void hideDialog(){
+        if(mProgressBar.getVisibility() == View.VISIBLE){
+            mProgressBar.setVisibility(View.INVISIBLE);
+        }
+    }
+
+    private void hideSoftKeyboard(){
+        this.getWindow().setSoftInputMode(WindowManager.LayoutParams.SOFT_INPUT_STATE_ALWAYS_HIDDEN);
+    }
+
+
+}
